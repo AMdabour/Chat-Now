91b4d6404e1cf92fb3f01bbe2c6394ea5b1be037
Abdelmoneim000 <ab000arabi22@gmail.com>
2024-03-19 16:22:42 +0200

    creating a chat window

---
+++ client/src/chatTest.js
@@ -0,0 +1,46 @@
+import {
+  Chat,
+  Channel,
+  ChannelList,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  useCreateChatClient,
+} from "stream-chat-react";
+import "stream-chat-react/dist/css/v2/index.css";
+
+const apiKey = "f5g6hxyeb5ae";
+const userId = "1";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMSJ9.S_o4GK1Bm86CyqP5nZn-K17wCw1uwAbzl0ihZEZPAqY";
+
+const filters = { members: { $in: [userId] }, type: "messaging" };
+const options = { presence: true, state: true };
+const sort = { last_message_at: -1 };
+
+const Hello = () => {
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: token,
+    userData: { id: userId },
+  });
+
+  if (!client) return <div>Loading...</div>;
+
+  return (
+    <Chat client={client}>
+      <ChannelList sort={sort} filters={filters} options={options} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default Hello;
